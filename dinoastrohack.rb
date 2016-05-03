4d1df9df1dc7f7dac7eba057cf55ddd46f3ab426
ImArg <agnicom@hotmail.com>
2016-05-03 16:20:43 -0400

    Ini - donegit status

--- dinoastrohack.rb
+++ dinoastrohack.rb
@@ -1,11 +1,16 @@
 class Astrodino
 
-def initialize
-  @energy=rand(10)
-  @backpack
-  @emotions= false
-  @name=gets.chomp
-  @space_food=rand(5)
-end
+  def initialize
+  
+    @energy = rand(2) + 2
+    #@inventory = 
+    @happy = false
+    puts"Put your astro-dinosaur's name: "
+    @name = gets.chomp
+    @space_food = rand(5)
+    @lunar_rock = rand(3) + 1
+    @gas = 5
+
+  end
 
 end
